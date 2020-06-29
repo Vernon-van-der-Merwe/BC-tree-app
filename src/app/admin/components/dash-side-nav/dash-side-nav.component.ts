9bedee3afa4bb8f9e105d05fd35f155e53739651
Vernon van der Merwe <Vernon.vd.m14@gmail.com>
2020-06-29 15:00:07 +0200

    add sidebar and admin pages

--- src/app/admin/components/dash-side-nav/dash-side-nav.component.ts
+++ src/app/admin/components/dash-side-nav/dash-side-nav.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dash-side-nav',
   templateUrl: './dash-side-nav.component.html',
-  styleUrls: ['./dash-side-nav.component.css']
+  styleUrls: ['./dash-side-nav.component.sass']
 })
 export class DashSideNavComponent implements OnInit {
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,22 @@ import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { DefaultComponent } from './landing/layout/default/default.component';
 import { WelcomeComponent } from './landing/pages/welcome/welcome.component';
 import { HomeComponent } from './landing/pages/home/home.component';
+import { DashLayoutComponent } from './admin/layout/dash-layout/dash-layout.component';
+import { AnalyticsComponent } from './admin/pages/analytics/analytics.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DefaultComponent,
-    children: [{ path: '', component: WelcomeComponent },{ path: 'home', component: HomeComponent }],
+    children: [
+      { path: '', component: WelcomeComponent },
+      { path: 'home', component: HomeComponent },
+    ],
+  },
+  {
+    path: 'admin',
+    component: DashLayoutComponent,
+    children: [{ path: '', component: AnalyticsComponent }],
   },
 ]; // sets up routes constant where you define your routes
 
